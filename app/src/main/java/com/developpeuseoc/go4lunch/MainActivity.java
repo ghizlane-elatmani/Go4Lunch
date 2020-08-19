2b37165606262226cdcced8f4f6f1e3160dd610d
Ghizlane El atmani <ghizlane.elatmani.99@gmail.com>
2020-08-19 18:20:33 +0200

    1.0.4 - Adding Google authentication in MainActivity.java

--- app/src/main/java/com/developpeuseoc/go4lunch/MainActivity.java
+++ app/src/main/java/com/developpeuseoc/go4lunch/MainActivity.java
@@ -3,12 +3,37 @@ package com.developpeuseoc.go4lunch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickGoogleLoginButton(View view){
+        this.startSignInActivity();
+    }
+
+    private void startSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build())
+                        )
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN
+        );
+    }
+
 }
