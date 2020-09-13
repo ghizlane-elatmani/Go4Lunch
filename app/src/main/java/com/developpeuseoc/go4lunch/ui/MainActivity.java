986de2581ecce6e6e693e3cfa87573b3ecd50580
Ghizlane El atmani <ghizlane.elatmani.99@gmail.com>
2020-09-13 11:10:54 +0200

    1.1.0 - Bottom navigation success

--- app/src/main/java/com/developpeuseoc/go4lunch/ui/MainActivity.java
+++ app/src/main/java/com/developpeuseoc/go4lunch/ui/MainActivity.java
@@ -1,10 +1,18 @@
 package com.developpeuseoc.go4lunch.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.developpeuseoc.go4lunch.R;
+import com.developpeuseoc.go4lunch.ui.fragment.ChatFragment;
+import com.developpeuseoc.go4lunch.ui.fragment.ListFragment;
+import com.developpeuseoc.go4lunch.ui.fragment.MapFragment;
+import com.developpeuseoc.go4lunch.ui.fragment.WorkmatesFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +20,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MapFragment()).commit();
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_map_view :
+                            selectedFragment = new MapFragment();
+                            break;
+
+                        case R.id.nav_list_view :
+                            selectedFragment = new ListFragment();
+                            break;
+
+                        case R.id.nav_workmates :
+                            selectedFragment = new WorkmatesFragment();
+                            break;
+
+                        case R.id.nav_chat :
+                            selectedFragment = new ChatFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
 }
